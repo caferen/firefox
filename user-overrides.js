bd39b8b32917bdea77a1aaf8a2206fec28748a05
Eren <eren.simsek@tuta.io>
2024-07-18 09:16:38 +0100

    init

---
+++ user-overrides.js
@@ -0,0 +1,15 @@
+user_pref("privacy.resistFingerprinting.letterboxing", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("network.trr.mode", 5);
+user_pref("signon.rememberSignons", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
+user_pref("identity.fxaccounts.account.device.name", "PC");
+user_pref("browser.contentblocking.category", "custom");
+user_pref("network.cookie.cookieBehavior", 1);
+user_pref("browser.startup.homepage", "about:home");
+user_pref("browser.startup.firstrunSkipsHomepage", false);
+user_pref("browser.startup.page", 1);
+user_pref("browser.newtabpage.enabled", true);
+user_pref("media.peerconnection.enabled", false);
+
+user_pref("_user.js.parrot", "SUCCESS: Overrides loaded");
